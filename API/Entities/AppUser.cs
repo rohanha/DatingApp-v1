82e73df182c95b5a83a5e1d77fb7c0950e389869
Rohan <rohanha@gmail.com>
2022-10-31 15:11:14 +0400

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,6 +9,8 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UseName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
     }
 }
